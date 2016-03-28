363716bd3bfd84a6a72396f0f64dc61cd1e4028c
Thormod Myrvang <thormdom@stud.ntnu.no>
2016-03-28 17:47:02 +0200

    Make interfaces

--- app/src/main/java/tdt4240/lyvlyvtjuesyv/GameHub.java
+++ app/src/main/java/tdt4240/lyvlyvtjuesyv/GameHub.java
@@ -1,7 +1,16 @@
 package tdt4240.lyvlyvtjuesyv;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by RayTM on 28.03.2016.
  */
-public class GameHub {
+public class GameHub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hub);
+    }
 }
--- app/src/main/java/tdt4240/lyvlyvtjuesyv/LocalServer.java
+++ app/src/main/java/tdt4240/lyvlyvtjuesyv/LocalServer.java
@@ -1,7 +1,21 @@
 package tdt4240.lyvlyvtjuesyv;
 
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by RayTM on 28.03.2016.
  */
-public class LocalServer {
+public class LocalServer extends IntentService {
+    public static final int DEFAULT_PORT = 9000;
+
+    public LocalServer() {
+        super("Lyv Lyv, Tjuesyv - Server");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent workIntent) {
+        String dataString = workIntent.getDataString();
+    }
 }
