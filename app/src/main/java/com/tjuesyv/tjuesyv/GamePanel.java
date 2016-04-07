51ed1ce20557b424cf2e2f31c7252faf26939aef
Vrudin <vegarru@gmail.com>
2016-04-07 12:30:00 +0200

    Make main game activity

---
+++ app/src/main/java/com/tjuesyv/tjuesyv/GamePanel.java
@@ -0,0 +1,7 @@
+package com.tjuesyv.tjuesyv;
+
+/**
+ * Created by vegarrudin on 07.04.2016.
+ */
+public class GamePanel {
+}
---
+++ app/src/main/java/com/tjuesyv/tjuesyv/MainGame.java
@@ -0,0 +1,10 @@
+package com.tjuesyv.tjuesyv;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vegarrudin on 07.04.2016.
+ */
+public class MainGame extends AppCompatActivity {
+
+}
